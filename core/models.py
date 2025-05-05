3449a43cc4a1f0223ad14ec468c9b3c991d54ecb
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2025-05-05 19:43:13 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -5,4 +5,12 @@ class Produto(models.Model):
     preço = models.DecimalField('Preço', decimal_places=2, max_digits=10)
     esstoque = models.IntegerField('Estoque')
     
+    def __str__(self):
+        return self.nome
+    
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    telefone = models.PositiveIntegerField('Telefone', max_length=11)
+    
+    
 
