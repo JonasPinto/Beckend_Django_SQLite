4fc313a085741e40c387aedf072b760335a165e2
Jonas da Silva Pinto <68355317+JonasPinto@users.noreply.github.com>
2025-05-05 21:49:13 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import Produto, Cliente
 
-admin.site.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+   list_display = ('nome', 'preco', 'estoque', 'cor')
+
+admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Cliente)
